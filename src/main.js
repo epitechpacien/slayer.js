4d437083a976689ed885aff06305511fe9dde306
epitechpacien <pacien.ilikujije@epitech.eu>
2024-06-19 14:21:10 +0200

    [DEL] plateau initialisation

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 import {settings} from "./init.js";
 
-const plateau_geo = new THREE.BoxGeometry(5, 0.1, 5);
-const plateau_material = new THREE.MeshPhongMaterial( {color: 0xb8b894} );
-const mesh_plateau = new THREE.Mesh(plateau_geo, plateau_material);
-
 const rengoku_loader = new GLTFLoader();
 let rengoku_model;
 let mixer;
@@ -14,7 +10,7 @@ let wave_clip;
 let action;
 
 let scene = settings.scene;
-rengoku_loader.load('animations/wave.glb', function (gltf) {
+rengoku_loader.load('animations/rengoku.glb', function (gltf) {
     rengoku_model = gltf.scene;
     rengoku_model.scale.set(0.3, 0.3, 0.3)
     rengoku_model.position.set(0, 0.6, 1);
@@ -22,7 +18,7 @@ rengoku_loader.load('animations/wave.glb', function (gltf) {
     scene.add(rengoku_model);
     mixer = new THREE.AnimationMixer(rengoku_model);
     rengoku_clips = gltf.animations;
-    wave_clip = THREE.AnimationClip.findByName(rengoku_clips, 'wave_action');
+    wave_clip = THREE.AnimationClip.findByName(rengoku_clips, 'ninja_run');
     action = mixer.clipAction(wave_clip);
     action.play();
     }, undefined, function (error) {
@@ -30,10 +26,6 @@ rengoku_loader.load('animations/wave.glb', function (gltf) {
     }
 );
 settings.scene = scene;
-
-mesh_plateau.position.set(0, 0.5, -0.1);
-mesh_plateau.rotation.x += 0.1;
-settings.scene.add(mesh_plateau);
 
 loop()
 
