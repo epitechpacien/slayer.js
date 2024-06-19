d0566055388353839c6511d1faba8c0bfb31210f
epitechpacien <pacien.ilikujije@epitech.eu>
2024-06-19 20:09:10 +0200

    [ADD] player class and delete init_palyer()

--- src/player.js
+++ src/player.js
@@ -1,33 +1,42 @@
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {settings} from "./init.js";
 
-function init_player(settings) {
-    const rengoku_loader = new GLTFLoader();
-    let rengoku_model;
-    let rengoku_clips;
-    let wave_clip;
-    let action;
+ class player_info {
+    constructor () {
+        this.model;
+        this.mixer;
+        this.run_clip;
+        this.jump_clip;
+        this.dash_clip;
+        this.action;
+    }
+ }
 
-    let mixer;
-    let scene = settings.scene;
-    rengoku_loader.load('animations/rengoku.glb', function (gltf) {
-        rengoku_model = gltf.scene;
-        rengoku_model.scale.set(0.3, 0.3, 0.3)
-        rengoku_model.position.set(0, 0.6, 1);
-        rengoku_model.rotation.y = Math.PI;
-        scene.add(rengoku_model);
-        mixer = new THREE.AnimationMixer(rengoku_model);
-        rengoku_clips = gltf.animations;
-        wave_clip = THREE.AnimationClip.findByName(rengoku_clips, 'ninja_run');
-        action = mixer.clipAction(wave_clip);
-        action.play();
-        }, undefined, function (error) {
-            console.error('An error occured while loading the model', error);
-        }
-    );
-    settings.scene = scene;
-    settings.mixer = mixer;
-    return settings;
-}
+/*create player class with all info*/
 
-export {init_player};
+let player = new player_info();
+const rengoku_loader = new GLTFLoader();
+let rengoku_clips;
+
+rengoku_loader.load('animations/rengoku.glb', function (gltf) {
+    player.model = gltf.scene;
+    player.model.scale.set(0.3, 0.3, 0.3)
+    player.model.position.set(0, 0.6, 1);
+    player.model.rotation.y = Math.PI;
+    settings.scene.add(player.model);
+    player.mixer = new THREE.AnimationMixer(player.model);
+    rengoku_clips = gltf.animations;
+    player.run_clip = THREE.AnimationClip.findByName(rengoku_clips, 'ninja_run');
+    player.jump_clip = THREE.AnimationClip.findByName(rengoku_clips, 'jump');
+    player.dash_clip = THREE.AnimationClip.findByName(rengoku_clips, 'dash');
+    player.action = player.mixer.clipAction(player.run_clip);
+    player.action.play();
+    }, undefined, function (error) {
+        console.error('An error occured while loading the model', error);
+    }
+);
+
+/*load player model with postion, scale, rotation and run clip*/
+
+export {settings, player};
